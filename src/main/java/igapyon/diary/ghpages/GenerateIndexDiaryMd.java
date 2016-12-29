1cf34d14fb0ef6defff23c27a58f90fc2062f0fd
Toshiki Iga <igapyon@gmail.com>
2016-12-29 18:44:54 +0900

    set add

--- src/main/java/igapyon/diary/ghpages/GenerateIndexDiaryMd.java
+++ src/main/java/igapyon/diary/ghpages/GenerateIndexDiaryMd.java
@@ -14,34 +14,37 @@ public class GenerateIndexDiaryMd {
 
 		if (dir.getName().equals("diary")) {
 			System.out.println("期待通りディレクトリ");
-			processDir(dir);
+			processDir(dir, "");
 		} else {
 			System.out.println("期待とは違うディレクトリ:" + dir.getName());
 			return;
 		}
 	}
 
-	void processDir(final File dir) throws IOException {
+	void processDir(final File dir, String path) throws IOException {
 		final File[] files = dir.listFiles();
 		if (files == null) {
 			return;
 		}
 		for (File file : files) {
 			if (file.isDirectory()) {
-				processDir(file);
+				processDir(file, path + "/" + file.getName());
 			} else if (file.isFile()) {
 				if (file.getName().startsWith("ig") && file.getName().endsWith(".md")
 						&& false == file.getName().endsWith(".src.md")) {
 					System.out.println(file.getName());
-					processFile(file);
+					processFile(file, path);
 				}
 			}
 		}
 	}
 
-	void processFile(final File file) throws IOException {
+	void processFile(final File file, final String path) throws IOException {
 		final List<String> lines = FileUtils.readLines(file, "UTF-8");
 
+		final String url = "https://igapyon.github.io/diary" + path + "/"
+				+ file.getName().substring(0, file.getName().length() - 3);
+System.out.println(url);
 		System.out.println(lines.get(0));
 	}
 }
