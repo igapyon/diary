a6e26548d1ae035986eb2ca08eeb4bf5f20f46ad
Toshiki Iga <igapyon@gmail.com>
2016-12-29 17:31:00 +0900

    add sources for diary.

---
+++ src/main/java/igapyon/diary/ghpages/App.java
@@ -0,0 +1,13 @@
+package igapyon.diary.ghpages;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
