32098b9a3ef5d53f6be501485844b7d06bf2f7e6
Toshiki Iga <igapyon@gmail.com>
2016-12-29 17:57:47 +0900

    save

--- src/main/java/igapyon/diary/ghpages/ConvertSrcMd2Md.java
+++ src/main/java/igapyon/diary/ghpages/ConvertSrcMd2Md.java
@@ -15,9 +15,26 @@ public class ConvertSrcMd2Md {
 
 		if (dir.getName().equals("diary")) {
 			System.out.println("期待通りディレクトリ");
+			processDir(dir);
 		} else {
 			System.out.println("期待とは違うディレクトリ:" + dir.getName());
 			return;
+		}
+	}
+
+	void processDir(final File dir) {
+		final File[] files = dir.listFiles();
+		if (files == null) {
+			return;
+		}
+		for (File file : files) {
+			if (file.isDirectory()) {
+				processDir(file);
+			} else if (file.isFile()) {
+				if (file.getName().endsWith(".src.md")) {
+					System.out.println(file.getName());
+				}
+			}
 		}
 	}
 }
