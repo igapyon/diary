7cb7368da102e25428f724cc0d75ccbae3c2f82a
Toshiki Iga <igapyon@gmail.com>
2016-12-29 17:50:30 +0900

    save

--- src/main/java/igapyon/diary/ghpages/ConvertSrcMd2Md.java
+++ src/main/java/igapyon/diary/ghpages/ConvertSrcMd2Md.java
@@ -3,9 +3,21 @@ package igapyon.diary.ghpages;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * java -jar target/IgapyonDiaryGhPages-jar-with-dependencies.jar
+ * 
+ */
 public class ConvertSrcMd2Md {
 	public void process() throws IOException {
 		File dir = new File(".");
-		System.out.println(dir.getCanonicalPath());
+		dir = dir.getCanonicalFile();
+		System.out.println(dir.getPath());
+
+		if (dir.getName().equals("diary")) {
+			System.out.println("期待通りディレクトリ");
+		} else {
+			System.out.println("期待とは違うディレクトリ:" + dir.getName());
+			return;
+		}
 	}
 }
