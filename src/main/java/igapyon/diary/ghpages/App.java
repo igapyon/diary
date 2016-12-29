17cadb9f8e19e26fc893b206a0151445de59421b
Toshiki Iga <igapyon@gmail.com>
2016-12-30 07:34:07 +0900

    coding.

--- src/main/java/igapyon/diary/ghpages/App.java
+++ src/main/java/igapyon/diary/ghpages/App.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import igapyon.diary.ghpages.html.GenerateIndexDiaryHtml;
+
 /**
  * mvn install exec:java
  */
@@ -15,6 +17,8 @@ public class App {
 			new ConvertDiarySrcMd2Md().process();
 
 			final List<DiaryItemInfo> diaryItemInfoList = new GenerateIndexDiaryMd().process();
+			final List<DiaryItemInfo> diaryItemInfoHtmlList = new GenerateIndexDiaryHtml().process();
+			diaryItemInfoList.addAll(diaryItemInfoHtmlList);
 
 			new ProcessIndexListing().process(new File("README.src.md"), diaryItemInfoList);
 			new ProcessIndexListing().process(new File("idxall.html.src.md"), diaryItemInfoList);
