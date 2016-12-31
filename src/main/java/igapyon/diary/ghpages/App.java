e86808377f118ab09c3eea6c0aee612d94598785
Toshiki Iga <igapyon@gmail.com>
2016-12-31 14:35:54 +0900

    save

--- src/main/java/igapyon/diary/ghpages/App.java
+++ src/main/java/igapyon/diary/ghpages/App.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
-import igapyon.diary.ghpages.html.GenerateIndexDiaryHtml;
 import igapyon.diary.ghpages.todaydiary.GenerateTodayDiary;
 import jp.igapyon.diary.v3.hatena.ConvertHatenaSeparatedText2SrcMd;
+import jp.igapyon.diary.v3.html.GenerateIndexDiaryHtml;
 import jp.igapyon.diary.v3.item.DiaryItemInfo;
 import jp.igapyon.diary.v3.item.DiaryItemInfoComparator;
 
