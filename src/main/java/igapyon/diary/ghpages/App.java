e8cda732f7706f0758535b51e060b76aac20379b
Toshiki Iga <igapyon@gmail.com>
2017-01-05 18:13:09 +0900

    save

--- src/main/java/igapyon/diary/ghpages/App.java
+++ src/main/java/igapyon/diary/ghpages/App.java
@@ -2,7 +2,6 @@ package igapyon.diary.ghpages;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,14 +12,13 @@ import com.rometools.rome.feed.synd.SyndFeedImpl;
 import com.rometools.rome.io.FeedException;
 import com.rometools.rome.io.SyndFeedOutput;
 
+import jp.igapyon.diary.v3.atom.DiaryIndexAtomGenerator;
 import jp.igapyon.diary.v3.gendiary.TodayDiaryGenerator;
 import jp.igapyon.diary.v3.hatena.HatenaText2SrcMdConverter;
-import jp.igapyon.diary.v3.html.IndexDiaryHtmlParser;
 import jp.igapyon.diary.v3.html2md.IgapyonV2Html2MdConverter;
 import jp.igapyon.diary.v3.item.DiaryItemInfo;
 import jp.igapyon.diary.v3.item.DiaryItemInfoComparator;
 import jp.igapyon.diary.v3.mdconv.DiarySrcMd2MdConverter;
-import jp.igapyon.diary.v3.mdconv.IndexDiaryMdParser;
 import jp.igapyon.diary.v3.util.IgapyonV3Settings;
 
 /**
@@ -58,6 +56,9 @@ public class App {
 				return;
 			}
 
+			// index用のatomファイルを生成および更新します。
+			new DiaryIndexAtomGenerator(settings).process(rootdir);
+
 			if (false) {
 				// v2 システムから v3 に html -> md 変換を実施します。
 				// ただし、このスクリプトは、日記移行時には活躍したものの、その後役目を終えて、通常は使われません。
@@ -78,67 +79,6 @@ public class App {
 			// .html.src.md ファイルから .md ファイルを生成します。
 			System.err.println("Convert .html.src.md to .html.md file.");
 			new DiarySrcMd2MdConverter(settings).processDir(rootdir);
-
-			{
-				// ルートディレクトリ用
-
-				// ファイルからファイル一覧情報を作成します。
-				System.err.println("Listing md files.");
-				final List<DiaryItemInfo> diaryItemInfoList = new IndexDiaryMdParser(settings).processDir(rootdir, "");
-				System.err.println("Listing html files.");
-				final List<DiaryItemInfo> diaryItemInfoHtmlList = new IndexDiaryHtmlParser(settings).processDir(rootdir,
-						"");
-				diaryItemInfoList.addAll(diaryItemInfoHtmlList);
-
-				// sort them
-				Collections.sort(diaryItemInfoList, new DiaryItemInfoComparator(true));
-
-				writeAtom(diaryItemInfoList, new File(rootdir, "atom.xml"), "Igapyon Diary v3 all");
-
-				{
-					int diaryListupCount = 15;
-
-					final List<DiaryItemInfo> recentItemInfoList = new ArrayList<DiaryItemInfo>();
-
-					for (DiaryItemInfo itemInfo : diaryItemInfoList) {
-						diaryListupCount--;
-						recentItemInfoList.add(itemInfo);
-						if (diaryListupCount <= 0) {
-							break;
-						}
-					}
-
-					writeAtom(recentItemInfoList, new File(rootdir, "atomRecent.xml"), "Igapyon Diary v3 recent");
-				}
-
-				// new ProcessIndexListing(settings).process(new
-				// File("README.src.md"), diaryItemInfoList);
-				// new ProcessIndexListing(settings).process(new
-				// File("idxall.html.src.md"), diaryItemInfoList);
-			}
-
-			final String[] YEARS = new String[] { "1996", "1997", "1998", "2000", "2001", "2002", "2003", "2004",
-					"2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016",
-					"2017" };
-
-			for (String year : YEARS) {
-				// 各年ディレクトリ用
-
-				// ファイルからファイル一覧情報を作成します。
-				System.err.println("Listing md files for :" + year);
-				final List<DiaryItemInfo> diaryItemInfoList = new IndexDiaryMdParser(settings)
-						.processDir(new File(rootdir, year), "/" + year);
-
-				System.err.println("Listing html files for :" + year);
-				final List<DiaryItemInfo> diaryItemInfoHtmlList = new IndexDiaryHtmlParser(settings)
-						.processDir(new File(rootdir, year), "/" + year);
-				diaryItemInfoList.addAll(diaryItemInfoHtmlList);
-
-				// sort them
-				Collections.sort(diaryItemInfoList, new DiaryItemInfoComparator(false));
-
-				writeAtom(diaryItemInfoList, new File(rootdir, year + "/atom.xml"), "Igapyon Diary v3 year " + year);
-			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
